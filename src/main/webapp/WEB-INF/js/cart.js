7c282acd4e2997e7d111840a0ba6ee50765e9411
vamour <vamour@b10p8.fil.univ-lille1.fr>
2018-09-11 10:45:20 +0200

    [VVA] Feat - Ajout des modules css externes automatiquement

--- src/main/webapp/WEB-INF/js/cart.js
+++ src/main/webapp/WEB-INF/js/cart.js
@@ -1,7 +1,13 @@
-var Cart;
-
+/**
+ * Create a new cart
+ * A cart is a collection of articles
+ * @param wsLink The link of webservices which manage the cart
+ * @param color The main color of the button. You can put a hex value or a plain text color (Ex. "orange" or "#5d875f") 
+ * @returns A new cart object
+ */
 function Cart(wsLink, color){
 	
+	// If there's no color, we put "orange" as default value
 	if(color == undefined) {
 		this.color = "orange";
 	} else {
@@ -10,6 +16,9 @@ function Cart(wsLink, color){
 	
 	this.wsLink = wsLink;
 	
+	/**
+	 * Add an article to the cart and update the button 
+	 */
 	this.addArticle = function(reference, nom, lien, prixUnite, quantite) {
 		const req = new XMLHttpRequest();
 
@@ -36,41 +45,30 @@ function Cart(wsLink, color){
 		req.send(JSON.stringify(body));
 	}
 	
+	/**
+	 * Remove one article from the cart
+	 */
 	this.removeArticle = function(reference) {
 		// TODO
 	}
 	
-	
-	this.initialization = function(){
-		// CRÉATION DE L'ARBRE DOM
-		var cartDom = document.getElementById("cart");
-		var cartButton = document.createElement("div");
-		var cartIcon = document.createElement("i");
-		var articlesQuantity = document.createElement("span");
-		var buttonText = document.createElement("span");
+	this.initializeStyle = function() {
 		
-		cartButton.id = "cart-button";
+		var bootstrap = document.createElement("link");
+		var fontwesome = document.createElement("link");
 		
-		cartIcon.className = "fas fa-shopping-cart";
-		cartIcon.id = "cart-icon";
-		cartButton.appendChild(cartIcon);
+		bootstrap.rel = "stylesheet";
+		fontawesome.rel = "stylesheet";
 		
-		articlesQuantity.id = "articles-quantity";
-		articlesQuantity.className = "badge";
-		articlesQuantity.innerText = "0";
-		cartButton.appendChild(articlesQuantity);		
+		bootstrap.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+		fontwesome.href = "https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+			
 		
-		buttonText.id = "button-text";
-		buttonText.innerText = "Votre panier";
+		document.querySelector("head").appendChild(fontawesome);
+		document.querySelector("head").appendChild(bootstrap);
 		
-		cartButton.appendChild(buttonText);
 		
-		// AJOUT D'ÉVÈNEMENTS
-		cartButton.addEventListener("click", function(){
-			alert()
-		})
-		
-		// STYLE DU BOUTON
+		// BUTTON STYLE
 		var cssText = `
 			#cart-button {
                 border: 1px solid #DDD;
@@ -118,13 +116,50 @@ function Cart(wsLink, color){
 			}
 
 			document.getElementsByTagName('head')[0].appendChild(style);
-			
-			// AFFICHAGE DU BOUTON
-			cartDom.appendChild(cartButton);
-	}.bind(this)
+	}
 	
 	/**
-	 * On charge le module
+	 * Initialize the cart button
+	 */
+	this.initialization = function(){
+		this.initializeStyle();
+		
+		// CREATION OF THE DOM TREE
+		var cartDom = document.getElementById("cart");
+		var cartButton = document.createElement("div");
+		var cartIcon = document.createElement("i");
+		var articlesQuantity = document.createElement("span");
+		var buttonText = document.createElement("span");
+		
+		cartButton.id = "cart-button";
+		
+		cartIcon.className = "fas fa-shopping-cart";
+		cartIcon.id = "cart-icon";
+		cartButton.appendChild(cartIcon);
+		
+		articlesQuantity.id = "articles-quantity";
+		articlesQuantity.className = "badge";
+		articlesQuantity.innerText = "0";
+		cartButton.appendChild(articlesQuantity);		
+		
+		buttonText.id = "button-text";
+		buttonText.innerText = "Votre panier";
+		
+		cartButton.appendChild(buttonText);
+		
+		// EVENTS
+		cartButton.addEventListener("click", function(){
+			alert()
+		})
+			
+		// BUTTON DISPLAY
+		cartDom.appendChild(cartButton);
+	}.bind(this)
+	
+	
+	
+	/**
+	 * We load the module
 	 */
 	window.addEventListener("load", this.initialization);
 	
